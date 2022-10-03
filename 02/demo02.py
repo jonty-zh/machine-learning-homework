0604991e005304c0bcb4b63cbd27af0406ef2bf1
jonty <jonty_zh@163.com>
2022-10-03 21:02:06 +0800

    feat: 编写基本子步骤

--- 02/demo02.py
+++ 02/demo02.py
@@ -1,7 +1,5 @@
 #encoding:utf-8
-from ctypes import sizeof
-from enum import unique
-from re import sub
+
 import pandas as pd
 import numpy as np 
 import collections
@@ -52,6 +50,22 @@ def ana_data(data):
         BaseClassify[label] = (data_count2[label]+1)/float(len(labels)+len(BaseClassify.keys()))
     print(BaseClassify)
 
+    label1 = data[:, 0]
+    cnt1 = collections.Counter(label1)
+    print(cnt1)
+
+    data1 = np.array(data)
+    classify1 = {}
+    for i in range(data1.shape[1]):
+        classify1[i] = dict()
+        tags = list(set(data[:, i]))
+        d = data1[:, i]
+        tag_count = collections.Counter(data1[:, i])
+        for tag in tags:
+            # 平滑处理
+            classify1[i][tag] = (tag_count[tag]+1)/float(len(d)+len(tags)) 
+    print("!!!!!!!!!!!!!!!!!!", classify1)
+
     NBClassify = dict.fromkeys(data_count2.keys()) # 
     for la in NBClassify.keys():
         NBClassify[la] = {}
@@ -70,7 +84,57 @@ def ana_data(data):
         # print(sub_data)
     # print(NBClassify)
     print(NBClassify['是'])
+
+    for i in range(len(NBClassify.keys())):
+        print(i, NBClassify.keys())
+    # test = ['浅白', '稍蜷', '浊响', '稍糊', '凹陷', '硬滑']
+    test = ['青绿','蜷缩','浊响','清晰','凹陷','硬滑']
+    tags = list(NBClassify.keys())
+
+    # 创建混淆矩阵
+    con_matrix = np.array([[0.1, 0.2], [0.3, 0.4]])
+    R = dict.fromkeys(data_count2.keys())
+    print("R ", R)
+    print("ddddddddddd")
+    for j, label in enumerate(tags):
+        R[label] = 0
+        sum = np.math.log(BaseClassify[label], 2)
+        for k, tag in enumerate(test):
+            # 对数似然避免下溢
+            print(j, k, tag, BaseClassify[label], NBClassify[label][k][tag], classify1[k][tag])
+            # sum += BaseClassify[label] * NBClassify[label][k][tag] / classify1[k][tag]
+            sum = sum + np.math.log(NBClassify[label][k][tag], 2) - np.math.log(classify1[k][tag], 2)
+        R[label] += 2 * sum 
+        print("--------result : ", sum)
+        print("R: ", R[label], R)
+    
     # 求分母各概率值
+
+def test_data(data):
+    predict_vec = list()
+    for sample in data:
+        for k, tag in enumerate(sample):
+            print(k, tag)
+    # for lable in classify1.keys():
+    #     for k, tag in enumerate(data):
+    #         pass        
+
+def testNB(data, PG, PB, NBClassify):
+    predict_vec = list()
+    for sample in data:
+        pg = np.math.log(PG, 2)
+        pb = np.math.log(PB, 2)
+        for label in NBClassify.keys():
+            for k, tag in enumerate(sample):
+                if label == '是':
+                    pg += np.math.log(NBClassify[label][k][tag], 2)
+                else :
+                    pb += np.math.log(NBClassify[label][k][tag], 2)
+        if pg > pb:
+            predict_vec.append('是')
+        else:
+            predict_vec.append('否')
+    return np.array(predict_vec)
 
 # 计算概率
 
@@ -80,3 +144,7 @@ def ana_data(data):
 if __name__ == '__main__':
     data = load_data()
     ana_data(data)
+    test = [['浅白', '稍蜷', '浊响', '稍糊', '凹陷', '硬滑'],]
+    for i in test[0]:
+        print(i)
+    test_data(test)
